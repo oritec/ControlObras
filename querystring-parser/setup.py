f6fd607038146b1eba20550b3e3396587eba5a28
oritec <aceron@oritec.cl>
2017-09-23 09:27:19 -0300

    ADD: Follow Up

---
+++ querystring-parser/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='querystring_parser',
+      version='1.2.3',
+      description='QueryString parser for Python/Django that correctly handles nested dictionaries',
+      author='bernii',
+      author_email='berni@extensa.pl',
+      url='https://github.com/bernii/querystring-parser',
+      packages=['querystring_parser'],
+      install_requires=[
+        "requests",
+        "six",
+	  ],
+)
