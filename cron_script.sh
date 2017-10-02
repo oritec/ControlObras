5743b0c925bf76c26b1ff97fdae873fa31bc4aff
oritec <aceron@oritec.cl>
2017-10-02 16:34:37 -0300

    ADD: Primera prueba para dumpdatabase

--- cron_script.sh
+++ cron_script.sh
@@ -5,5 +5,4 @@ cd /home/andres/ControlObras
 source  /home/andres/Env/SaroenGlobal/bin/activate
 
 python dumpDatabase.py
-#find /home/andres/ControlObras/backups/ -mtime +10 -type f -delete
-find /home/andres/ControlObras/backups/ -type f -delete
+find /home/andres/ControlObras/backups/ -mtime +30 -type f -delete
